e1449bae98db238ebfa56f510ebcf39ea48caddb
Leonardo15 <602228173@qq.com>
2016-06-21 23:34:29 +0800

    TestBoolean

---
+++ Teach/src/Day06_01/TestBoolean.java
@@ -0,0 +1,12 @@
+package Day06_01;
+
+public class TestBoolean {
+		public static void main(String[] args) {
+			boolean b = true;
+			if(b){
+				System.out.println("b="+b+"满足条件执行");
+			}else{
+				System.out.println("不满足条件执行");
+			}
+		}
+}
