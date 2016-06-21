7c3f126afc16aa182f56a4c868acb675ce8f2ae7
Leonardo15 <602228173@qq.com>
2016-06-21 23:29:25 +0800

    TestChar

---
+++ Teach/src/Day06_01/TestChar.java
@@ -0,0 +1,19 @@
+package Day06_01;
+/**
+ * 
+ *
+ * @author Leonardo15
+ *
+ */
+public class TestChar {
+		public static void main(String[] args) {
+			char c1 ='a';
+			char c2 ='a'+1;
+			char c3 ='a'-1;
+			System.out.println("char=£º"+c1);
+			System.out.println("char=£º"+c2);
+			System.out.println("char=£º"+c3);
+			
+			
+		}
+}
