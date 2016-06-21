30164d909e6833d6688bb3ba11f14bae54af3e51
Leonardo15 <602228173@qq.com>
2016-06-21 23:22:09 +0800

    Float

---
+++ Teach/src/Day06_01/TestFloat.java
@@ -0,0 +1,25 @@
+package Day06_01;
+
+/**
+ * 
+ * 
+ * 
+ * @author Leonardo15
+ *
+ */
+public class TestFloat {
+		
+		public static void main(String[] args) {
+			int i1 = 1;
+			int i2 = 2;
+			float f1 = 1f;
+			float f2 = 3f;
+			float result1 , result2;
+			result1= i1/i2;
+			result2 = f1/f2;
+			System.out.println(result1);
+			System.out.println(result2);
+		}
+	
+	
+}
